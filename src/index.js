bf9aa2569be463bfb7f59dfe066499e9384ed660
Nikki Bhalodiya <nikkibhalodiya03@gmail.com>
2020-12-14 23:10:15 +0530

    setting up files

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import { React, Component } from 'react';
+import ReactDom from 'react-dom';
+
+import './index.css';
+
+
+class Square extends Component {
+    render() {
+        return (
+            <button className="square">Square</button>
+        )
+    };
+
+}
+
+class Board extends Component {
+    renderSquare(i) {
+        return <Square />
+    }
+
+    render() {
+        const status = 'Next Player - X'
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            <div className="game-info">
+                <div>{"___STATUS___"}</div>
+                <ol>{"__TODO__"}</ol>
+            </div>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <Game />,
+    document.getElementById('root')
+);
